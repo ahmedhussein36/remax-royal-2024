b6b4e63f492b503c860b8069f6a0cf6b40fc5f8e
Ahmed Hussein <105352918+ahmedhussein36@users.noreply.github.com>
2024-02-06 18:47:37 +0200

    commit 22

--- app/components/header/Logo.tsx
+++ app/components/header/Logo.tsx
@@ -1,14 +1,17 @@
 import Image from "next/image";
+import Link from "next/link";
 export default function Logo() {
     return (
         <div>
-            <Image
-                src="/assets/images/royal-logo.png"
-                alt="remaxroyal_logo"
-                width={100}
-                height={100}
-                // loading="lazy"
-            ></Image>
+            <Link href={"/"}>
+                <Image
+                    src="/assets/images/royal-logo.png"
+                    alt="remaxroyal_logo"
+                    width={100}
+                    height={100}
+                    // loading="lazy"
+                />
+            </Link>
         </div>
     );
 }

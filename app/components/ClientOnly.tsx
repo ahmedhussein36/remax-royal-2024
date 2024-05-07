1d1752b3329a848ddc92e97b405389598d2165ed
Ahmed Hussein <105352918+ahmedhussein36@users.noreply.github.com>
2024-05-07 14:43:46 +0300

    Update ClientOnly.tsx

--- app/components/ClientOnly.tsx
+++ app/components/ClientOnly.tsx
@@ -4,7 +4,7 @@ import React, { useState, useEffect, ReactNode } from "react";
 
 interface ClientOnlyProps {
     children: React.ReactNode;
-    fallback: React.ReactNode | null;
+    fallback?: React.ReactNode | null;
 }
 
 const ClientOnly: React.FC<ClientOnlyProps> = ({ children }, { fallback }) => {

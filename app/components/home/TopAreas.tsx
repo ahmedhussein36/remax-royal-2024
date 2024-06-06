9ff954381c4d0d8ed26c02959b21d487192c10ae
Ahmed Hussein <105352918+ahmedhussein36@users.noreply.github.com>
2024-06-07 01:55:37 +0300

    Update TopAreas.tsx

--- app/components/home/TopAreas.tsx
+++ app/components/home/TopAreas.tsx
@@ -19,22 +19,23 @@ const TopAreas = ({ areas }: { areas: SafeArea[] }) => {
                 {areas.slice(0, 8).map((area) => (
                     <div
                         className=" flex flex-col
-                                justify-start items-center gap-3 overflow-hidden 
+                                justify-start items-center gap-3 overflow-hidden py-8
                                 cursor-pointer border-neutral-300 border
                                 rounded-xl  hover:shadow-md transition-all duration-300 
                                 "
                         onClick={() => router.push(`area/${area.slug}`)}
                         key={area.id}
                     >
-                        <div className="flex justify-center items-center border relative w-[100px] h-[100px] rounded-full overflow-hidden">
+                        <div className="flex justify-center relative items-center border w-[80px] h-[80px] rounded-full overflow-hidden">
                             <Image
                                 src={area?.image || ""}
-                                width={300} height={200}
-                                sizes=""
+                                // width={300} height={200} 
+                                fill
+                                sizes="cover"
                                 alt={area.title}
                             />
                         </div>
-                        <span className=" text-slate-700">{area.title}</span>
+                        <span className=" font-bold text-slate-700">{area.title}</span>
                     </div>
                 ))}
             </div>
